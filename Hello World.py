f191aac5ad96c3808d7a5f01bd24f1353ba1bbb1
Krogs <66489697+Kroggieboy57@users.noreply.github.com>
2021-04-15 19:17:05 +0530

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,4 @@
+print("Hello World")
+x=2
+if x==1:
+    print("X is 1")
---
+++ Variables and types.py
@@ -0,0 +1,16 @@
+myint=7
+print(myint)
+
+myfloat=7.0
+print(myfloat)
+myfloat=float(7)
+print(myfloat)
+
+mystring='POG'
+mystring1="POGDANCE"
+print(mystring)
+print(mystring1)
+
+one=1
+two=2
+three=one+two
