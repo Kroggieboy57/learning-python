274da245f992fb6607f47f12f09a024763c41490
Krogs <>
2021-05-10 14:21:12 +0000

    learned new stuff

---
+++ loops.py
@@ -0,0 +1,7 @@
+name="Kroggieboy57"
+
+for character in name:
+  print(character)
+number=[1,2,3,4,5,6,7,8,9,0]
+for i in number:
+  print(i)
---
+++ sets.py
@@ -0,0 +1,8 @@
+s=set()
+s.add(1)
+s.add(3)
+s.add(5)
+s.add(3)
+s.remove(3)
+print (s)
+print(f"This set has {len(s)} numbers")
